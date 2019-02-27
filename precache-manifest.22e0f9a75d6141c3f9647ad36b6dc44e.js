fd67a59f31e7c417efbae5599584bbd84eb8cdf4
Ralf Popescu <ralf@landinglion.com>
2019-02-27 18:02:34 -0500

    Updates

---
+++ precache-manifest.22e0f9a75d6141c3f9647ad36b6dc44e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b19591092447e1a1c822",
+    "url": "/vedado-site/static/css/main.8adee0bd.chunk.css"
+  },
+  {
+    "revision": "b19591092447e1a1c822",
+    "url": "/vedado-site/static/js/main.b1959109.chunk.js"
+  },
+  {
+    "revision": "bd1f966e6d2065c88129",
+    "url": "/vedado-site/static/js/runtime~main.bd1f966e.js"
+  },
+  {
+    "revision": "69d08c018a01395b36db",
+    "url": "/vedado-site/static/js/2.69d08c01.chunk.js"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/vedado-site/static/media/twitter.7e145c9e.svg"
+  },
+  {
+    "revision": "bb026c4d996a5d563f7fceb5166175e1",
+    "url": "/vedado-site/static/media/instagram.bb026c4d.svg"
+  },
+  {
+    "revision": "efd20c2b5baf37d8fea16ed218facd02",
+    "url": "/vedado-site/static/media/soundcloud.efd20c2b.svg"
+  },
+  {
+    "revision": "f25eb9513141bd4fe377fdca5f97fe37",
+    "url": "/vedado-site/static/media/spotify.f25eb951.svg"
+  },
+  {
+    "revision": "d1ac35895ee9029b5218f669ea9f3400",
+    "url": "/vedado-site/static/media/facebook.d1ac3589.svg"
+  },
+  {
+    "revision": "ff90579998a22062d6255aabb058ab19",
+    "url": "/vedado-site/index.html"
+  }
+];
